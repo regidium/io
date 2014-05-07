15ff671f5e640fd2b002ba3a1d10a883466d8c87
Alexey Volkov <alexey.wild88@gmail.com>
2014-05-07 23:38:26 +0300

    fix

--- src/app/socket/sockets/service.js
+++ src/app/socket/sockets/service.js
@@ -18,16 +18,16 @@ var self = module.exports = function (io, socket, events)
                 events.publish('chat:disconnect', { chat_uid: socket.chat_uid, widget_uid: socket.widget_uid });
                 // Удаляем таймер
                 delete io.timers['chat_' + socket.chat_uid];
-            }, 1000);
+            }, 3000);
         } else if (socket.agent_uid) {
-            io.timers['agent_'+socket.chat_uid] = setTimeout(function() {
+            io.timers['agent_'+socket.agent_uid] = setTimeout(function() {
                 // Оповещаем пользователей
                 socket.broadcast.to(socket.widget_uid).emit('agent:disconnect', { agent_uid: socket.agent_uid, widget_uid: socket.widget_uid });
                 // Оповещаем event сервер об отключении агента
                 events.publish('agent:disconnect', { agent_uid: socket.agent_uid, widget_uid: socket.widget_uid });
                 // Удаляем таймер
                 delete io.timers['chat_' + socket.chat_uid];
-            }, 1000);
+            }, 60000);
         }
     });
 
